0b3d03b9a5db5eec4b5907d34682a11a83be298d
Marcus Boyd <mwb@geosol.com.au>
2019-08-07 16:43:33 +0930

    pythonPackages.tinydb: init at v3.14.1

---
+++ pkgs/development/python-modules/tinydb/default.nix
@@ -0,0 +1,36 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, pytest
+, pytestcov
+, pytestrunner
+, pycodestyle
+, pyyaml
+}:
+
+buildPythonPackage rec {
+  pname = "tinydb";
+  version = "v3.14.1";
+
+  src = fetchFromGitHub {
+    owner = "msiemens";
+    repo = pname;
+    rev = version;
+    sha256 = "02idbvrm8j4mwsjfkzy11f4png19k307p53s4qa2ifzssysxpb96";
+  };
+
+  checkInputs = [
+    pytest
+    pytestcov
+    pytestrunner
+    pycodestyle
+    pyyaml
+  ];
+
+  meta = with lib; {
+    description = "A lightweight document oriented database written in pure Python with no external dependencies";
+    homepage = https://github.com/msiemens/tinydb;
+    license = licenses.asl20;                                                                                                                                                                      
+    maintainers = [ maintainers.marcus7070 ];
+  };
+}
